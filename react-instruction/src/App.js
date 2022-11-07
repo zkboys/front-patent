c75866eb7dc398b4f68ecc93837987ee0c7e4988
zkboys <876213774@qq.com>
2022-11-07 15:41:38 +0800

    :art: 整理代码

--- react-instruction/src/App.js
+++ react-instruction/src/App.js
@@ -2,24 +2,25 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const active = true;
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img r-code="ABC" src={logo} className="App-logo" alt="logo"/>
+                <p className={['text', active && 'red']}>
+                    Edit <code>src/App.js</code> and save to reload.
+                </p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+            </header>
+        </div>
+    );
 }
 
 export default App;
---
+++ react-instruction/src/commons/index.js
@@ -0,0 +1,5 @@
+export function hasPermission(code) {
+    // TODO 基于code，判断用户是否有权限
+    // TODO 测试数据，50%概率有权限
+    return Math.random() > 0.5;
+}
